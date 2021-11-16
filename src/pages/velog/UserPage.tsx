6724e178403e6838aec16166322e4783377a516c
yeojini <yesjin1209@gmail.com>
2021-11-16 13:09:16 +0900

    [feat] callApi, Userpage 내 테스트 코드 삭제
    -

--- src/pages/velog/UserPage.tsx
+++ src/pages/velog/UserPage.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect } from 'react';
-import { callApi } from '../../api/api';
+import React from 'react';
 
 const UserPage = () => {
-  useEffect(() => {
-    callApi({ method: 'get', url: '/users' })
-      .then((response) => {
-        console.log(response);
-      })
-  }, []);
-
   return <div>소개 페이지 입니다.</div>;
 };
 
