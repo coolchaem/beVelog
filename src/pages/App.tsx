530a5cb3c35602803db69d30e7efcd8f4c59446c
yeojini <yesjin1209@gmail.com>
2021-11-21 19:05:11 +0900

    [feat] Header 임시 구현
    - velog 홈 이동 link 연결한 logo image 추가
    - Header 에 포함되어야 하는 버튼 임시로 추가

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import VelogPage from './velog/VelogPage';
 import ReadingListPage from './readingList/ReadingListPage';
-import RecentPage from './home/RecentPage';
+import RecentPostsPage from './home/RecentPostsPage';
 import SavesPage from './SavesPage';
 import SettingPage from './SettingPage';
 import WritePage from './home/PostPage';
 import HomePage from './home/HomePage';
 import { Counter } from '../components/molecules/Counter';
+import Header from '../components/organisms/main/Header';
 
 const App = () => {
   const userId = 'yeojin';
   return (
     <>
-      <h3>bevelog</h3>
       <Counter />
       <BrowserRouter>
+        <Header />
         <ul>
           <li>
             <Link to="/">트렌딩</Link>
@@ -38,16 +39,16 @@ const App = () => {
           <li>
             <Link to="/setting">설정</Link>
           </li>
-          <Route exact path="/" component={HomePage} />
-          <Switch>
-            <Route path="/@:userId" component={VelogPage} />
-            <Route path="/recent" component={RecentPage} />
-            <Route path="/write" component={WritePage} />
-            <Route path="/saves" component={SavesPage} />
-            <Route path="/lists" component={ReadingListPage} />
-            <Route path="/setting" component={SettingPage} />
-          </Switch>
         </ul>
+        <Route exact path="/" component={HomePage} />
+        <Switch>
+          <Route path="/@:userId" component={VelogPage} />
+          <Route path="/recent" component={RecentPostsPage} />
+          <Route path="/write" component={WritePage} />
+          <Route path="/saves" component={SavesPage} />
+          <Route path="/lists" component={ReadingListPage} />
+          <Route path="/setting" component={SettingPage} />
+        </Switch>
       </BrowserRouter>
     </>
   );
