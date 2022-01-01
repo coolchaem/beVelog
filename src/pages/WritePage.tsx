fde9b89aed44a168039f566c4ab13a83292676e9
youngjewoo <youngjewoo@naver.com>
2022-01-01 19:04:56 +0900

    [feat] 작성 페이지 프로토타입
    - writePage에서 helmet을 통해 브라우저 탭에 보여지는 title 태그 수정

--- src/pages/WritePage.tsx
+++ src/pages/WritePage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const WritePage = () => {
-  return <div>새 글 작성 페이지 입니다.</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>새 글 작성하기 | velog</title>
+      </Helmet>
+      <div>새 글 작성 페이지 입니다!</div>
+    </div>
+  );
 };
 
 export default WritePage;
