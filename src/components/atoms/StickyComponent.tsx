1064d85b89c4088adb9d45c76250cf11078bd843
coolchaem <coals6751@gmail.com>
2022-02-16 15:37:48 +0900

    [feat] sticky component fixed position 설정 - position fiexd로 결정 -- relative의 경우 주변 레이아웃 변경 시 동시 레이아웃 됨 - center align items 적용 - color 기본 회색 계열 추가와 string으로 color 설정 추가 - 위치는 top, left 값으로만 정하도록 수정 -- left + right 둘 다 주면 width가 늘어나므로 top, left vs bottom, right 둘 중 하날 선택함

--- src/components/atoms/StickyComponent.tsx
+++ src/components/atoms/StickyComponent.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 interface StickyProps {
   top: number;
+  left: number;
+  backgroudColor?: string;
   className?: string;
 }
 
-const StickyComponent = (prop: React.PropsWithChildren<StickyProps>) => {
-  return <div className={`${prop.className} ${{ top: prop.top }}`}>{prop.children}</div>;
+const StickyComponent = (props: React.PropsWithChildren<StickyProps>) => {
+  return (
+    <StickyBlock className={props.className} top={props.top} left={props.left}>
+      {props.children}
+    </StickyBlock>
+  );
 };
+
+const StickyBlock = styled.div<StickyProps>`
+  position: fixed;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
+  border: 1px solid rgb(241, 243, 245);
+  border-radius: 2rem;
+  align-items: center;
+  -webkit-box-align: center;
+  background-color: ${props => props.backgroudColor || 'rgb(248, 249, 250)'};
+`;
 
 export default StickyComponent;
