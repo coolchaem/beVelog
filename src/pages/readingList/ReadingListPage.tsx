f48cd66b459250067a05f293e474d8d444a44258
coolchaem <coals6751@gmail.com>
2022-04-11 22:23:35 +0900

    [feat] reading list page 틀 추가 - page 틀 추가

--- src/pages/readingList/ReadingListPage.tsx
+++ src/pages/readingList/ReadingListPage.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import PostCardGrid from '../../components/molecules/common/PostCardGrid';
+import PostCardGridSkeleton from '../../components/molecules/common/PostCardGridSkeleton';
+import useLoadPost from '../../hooks/useLoadPost';
+import { useAppSelector } from '../../redux/hooks';
 
 const ReadingListPage = () => {
-  //좋아한 포스트, 최근 읽은 포스트
-  return <div>읽기 글 목록 페이지 입니다.</div>;
+  // TODO 최근 읽은 포스트
+  const { username } = useAppSelector(state => state.userState);
+  const [targetRef, isLoading, isDone, posts] = useLoadPost(`${username}/readingList`);
+
+  return (
+    <>
+      <PostCardGrid posts={posts} />
+      {isLoading && !isDone && <PostCardGridSkeleton />}
+      {!isLoading && (
+        <div
+          ref={targetRef}
+          style={{
+            width: '100%',
+            height: '5rem',
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default ReadingListPage;
